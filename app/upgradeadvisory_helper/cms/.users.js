1f1ef7942dcdd244dc7634beedebf4057a859e77
unknown <andrey.trok@gmail.com>
2018-03-12 12:48:36 -0700

    merge with master

---
+++ app/upgradeadvisory_helper/cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'ec2929f1b562168e51dd20266de826ec',
+			hash: '4a68c7ea0abe84ee9e6ea0aec7e88190c2a038f82d207835ddb6fde22b4d5799',
+			user_created_timestamp: 1520643316355
+		}
+	]
+}
